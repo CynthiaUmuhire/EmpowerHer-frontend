bc32601acc0dc4137079c48c5d28181e376adc92
CynthiaUmuhire <umuhirecynthia64@gmail.com>
2025-04-24 08:56:44 +0200

    set up

---
+++ src/components/errorBoundary/errorBoundary.tsx
@@ -0,0 +1,26 @@
+import React, { ErrorInfo, ReactNode } from 'react';
+import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary';
+
+interface Props {
+    children: ReactNode;
+}
+
+const ErrorFallback = () => {
+    return <h1>Something went wrong.</h1>;
+};
+
+const ErrorBoundary: React.FC<Props> = ({ children }) => {
+    const handleError = (error: Error, errorInfo: ErrorInfo) => {
+        console.error('ErrorBoundary caught an error: ', error, {
+            componentStack: errorInfo.componentStack || 'No stack trace available',
+        });
+    };
+
+    return (
+        <ReactErrorBoundary FallbackComponent={ErrorFallback} onError={handleError}>
+            {children}
+        </ReactErrorBoundary>
+    );
+};
+
+export default ErrorBoundary;
