e373155982a898d8ddb636fe3ec313004ea9bbc4
CynthiaUmuhire <umuhirecynthia64@gmail.com>
2025-06-20 16:41:45 +0200

    feat: Implement role selection in login process and create RoleSelector component

--- src/components/ui/login.tsx
+++ src/components/ui/login.tsx
@@ -2,14 +2,17 @@ import { Link, useNavigate } from "react-router-dom"
 import EHInput from "./EHInput"
 import Links from "@/routes/Links"
 import CustomButton from "./customButton"
-import { useEffect, useRef } from "react";
-import api from "@/api/api";
+import { useEffect, useRef, useState } from "react";
 import useUserLogin from "@/hooks/useUserlogin";
 import Spinner from "./spinner";
+import RoleSelector from "./roleselector";
+import { BACKEND_URL } from "@/config";
 
 function Login() {
     const identifierRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
+    const [currentStep, setCurrentStep] = useState(1);
+    const [role, setRole] = useState<string | null>(null)
     const navigate = useNavigate();
     const { login, isPending, isError, isSuccess } = useUserLogin();
     const handleLogin = async () => {
@@ -26,6 +29,14 @@ function Login() {
             alert("Login failed. Please check your credentials.");
         }
     }
+    const handleContinueToLogin = () => {
+        if (role === 'mother') {
+            setCurrentStep(2);
+        }
+        if (role === 'facilitator') {
+            window.location.href = `${BACKEND_URL}/admin/auth/login`
+        }
+    };
     useEffect(() => {
         if (isSuccess) {
             console.log("Login successful");
@@ -34,20 +45,29 @@ function Login() {
     }, [isSuccess, navigate]);
     return (
         <div className='flex flex-col w-full gap-6'>
-            <div className="flex flex-col gap-6 ">
-                <EHInput placeholder="Enter your phone number" label={'Your phone number'} type='tel' ref={identifierRef} />
-                <EHInput placeholder="Password" label={'Your Password'} type="password" ref={passwordRef} />
-            </div>
-            <div className="flex justify-between">
-                <Link to={Links.auth.Register} className="text-secondary-400 text-sm font-light">No account yet? <strong>Register here</strong></Link>
-                <Link to={'/forgot-password'} className="text-secondary-400 text-sm font-light">Forgot Password?</Link>
-            </div>
-            <CustomButton variant={'secondary'} onClick={handleLogin} disabled={isPending}>
-                Login
-                {isPending && <Spinner />}
-            </CustomButton>
+            {currentStep === 1 && (
+                <RoleSelector
+                    setRole={setRole}
+                    onContinue={handleContinueToLogin}
+                />
+            )}
+            {currentStep === 2 && (
+                <><div className="flex flex-col gap-6 ">
+                    <EHInput placeholder="Enter your phone number" label={'Your phone number'} type='tel' ref={identifierRef} />
+                    <EHInput placeholder="Password" label={'Your Password'} type="password" ref={passwordRef} />
+                </div><div className="flex justify-between">
+                        <Link to={Links.auth.Register} className="text-secondary-400 text-sm font-light">No account yet? <strong>Register here</strong></Link>
+                        <Link to={'/forgot-password'} className="text-secondary-400 text-sm font-light">Forgot Password?</Link>
+                    </div><CustomButton variant={'secondary'} onClick={handleLogin} disabled={isPending}>
+                        Login
+                        {isPending && <Spinner />}
+                    </CustomButton></>
+            )}
+
+
         </div>
     )
 }
 
 export default Login
+
---
+++ src/components/ui/roleselector.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import CustomButton from './customButton';
+import { on } from 'events';
+
+interface RoleSelectorProps {
+    setRole: React.Dispatch<React.SetStateAction<string | null>>;
+    onContinue: () => void;
+}
+
+function RoleSelector({ setRole, onContinue }: RoleSelectorProps) {
+    const handleRoleSelection = (role: string) => {
+        setRole(role);
+        onContinue();
+    };
+    return (
+        <div className='flex flex-col w-full gap-6 items-center p-4 border rounded-lg shadow-md'>
+            <h2 className="text-xl font-semibold text-secondary-500">Login as a:</h2>
+            <CustomButton
+                variant={'outline'}
+                onClick={() => handleRoleSelection('mother')}
+                className="mt-4"
+            >
+                Mother
+            </CustomButton>
+            <CustomButton
+                variant={'outline'}
+                onClick={() => handleRoleSelection('facilitator')}
+                className="mt-4"
+
+            >
+                Facilitator
+            </CustomButton>
+        </div>
+    );
+}
+
+export default RoleSelector;
