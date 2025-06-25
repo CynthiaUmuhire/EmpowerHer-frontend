fcdc3294487b29aa4694fefa3328db834e2e1bfa
CynthiaUmuhire <umuhirecynthia64@gmail.com>
2025-06-25 05:11:11 +0200

    feat: update React Query dependencies to latest versions and modify routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ export default function App() {
       element: <MainLayout />,
       children: [
         {
-          path: Links.protected.Home,
+          index: true,
           element: <Home />,
         },
         {
